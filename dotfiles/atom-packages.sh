8e8801a09f763157438e1464984759ab45751d91
Sighery <Sighery@users.noreply.github.com>
2019-10-03 03:17:48 +0200

    Add Atom packages and themes data
    This commit adds my list of current packages and themes I use for
    Atom. It also adds a helper script that both updates these packages
    and themes files, and installs them.
    
    The install bit could be done with the base `apm` command, but, after
    some testing, it looks like it will install all packages regardless of
    whether they are already installed, and there's no flag or command
    option to avoid this behaviour, so I've had to wrap that behaviour
    into the script for the moment.

---
+++ dotfiles/atom-packages.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+OPERATION=$1
+BASEDIR=$2
+
+PACKAGES=`apm list --installed --packages --bare`
+THEMES=`apm list --installed --themes --bare`
+
+if [ -z "$BASEDIR" ]; then
+	BASEDIR="./"
+elif [ "${BASEDIR: -1}" != "/" ]; then
+	BASEDIR+="/"
+fi
+
+PACKAGES_FILE="${BASEDIR}atom-packages.list"
+THEMES_FILE="${BASEDIR}atom-themes.list"
+
+if [ "$OPERATION" == "install" ]; then
+	files=("$PACKAGES_FILE" "$THEMES_FILE")
+	for file in "${files[@]}"; do
+		# Last lines don't necessarily always include a newline
+		grep . "${file}" | while read package; do
+			package=`printf "$package" | cut -d"@" -f1`
+
+			if [ ! -d "$HOME/.atom/packages/$package" ]; then
+				printf "Installing package $package...\n"
+				apm install "$package"
+			fi
+		done
+	done
+else
+	printf "$PACKAGES" > "$PACKAGES_FILE"
+	printf "$THEMES" > "$THEMES_FILE"
+fi
