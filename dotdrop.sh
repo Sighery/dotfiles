af0f52949e10f303b0b35d5079c00fb1d86bf3fe
Sighery <Sighery@users.noreply.github.com>
2019-12-25 02:17:22 +0100

    Only check for dotdrop updates on non-sudo runs
    I tend to prefer SSH for my repositories. When executing dotdrop.sh as
    root it would then use different key files and it'd never work. Since
    it's generally very unusual to run the system configs, I'd rather just
    the script not try to autoupdate on sudo runs than change my commands.

--- dotdrop.sh
+++ dotdrop.sh
@@ -24,7 +24,7 @@ sub="dotdrop"
 # pivot
 cd "${cur}" || { echo "Directory \"${cur}\" doesn't exist, aborting." && exit 1; }
 # init/update the submodule
-if [ "${DOTDROP_AUTOUPDATE-yes}" = yes ] ; then
+if [ $EUID -ne 0 ] && [ "${DOTDROP_AUTOUPDATE-yes}" = yes ] ; then
   git submodule update --init --recursive
   git submodule update --remote dotdrop
 fi
