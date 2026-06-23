e4e4d6159ad37d72231d82a21ddeefa1f55f24b1
Sighery <11218602+Sighery@users.noreply.github.com>
2026-06-24 00:15:30 +0200

    Update keepassxc configuration
    Adding some extra flags the default install seems to use, and the most
    important is enabling the save backup files.
    
    I plan on using Syncthing going forward for my KeePass DBs, and for that
    these backup files can come in handy. Just in case.

--- home-sighery/keepass.nix
+++ home-sighery/keepass.nix
@@ -5,17 +5,28 @@
     enable = true;
 
     settings = {
-      Browser.Enabled = false;
+      Browser = {
+        Enabled = false;
+        CustomProxyLocation = "";
+      };
 
       General = {
+        ConfigVersion = 2;
+
         # Ctrl + Alt + V
         GlobalAutoTypeKey = 86;
         GlobalAutoTypeModifiers = 201326592;
+
         AutoTypeHideExpiredEntry = true;
+
+        BackupBeforeSave = true;
+        BackupFilePathPattern = "{DB_FILENAME}.{TIME}.kdbx";
       };
 
       GUI = {
         ShowExpiredEntriesOnDatabaseUnlock = false;
+
+        TrayIconAppearance = "monochrome-light";
       };
     } // inputs.dotfiles-secrets.extraKeepassxcSettings;
   };
