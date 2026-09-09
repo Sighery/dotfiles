ce498fe89567790bffdfe38dcbb6f7100503d870
Sighery <11218602+Sighery@users.noreply.github.com>
2026-09-09 19:47:42 +0200

    Sonar: Increase VSCode default zoom

--- home/sighery-sonar/main.nix
+++ home/sighery-sonar/main.nix
@@ -8,6 +8,10 @@
     ./i3.nix
   ];
 
+  programs.vscodium.profiles.default.userSettings = {
+    "window.zoomLevel" = 1;
+  };
+
   programs.ssh.settings = secrets.sonar.work_git_ssh;
   programs.git.includes = [ secrets.sonar.work_git_git ];
 }
